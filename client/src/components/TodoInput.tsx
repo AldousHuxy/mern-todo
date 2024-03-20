0c4dca07aa27f641587e79af4b28d39fc6e77753
Justin Howard <47066746+AldousHuxy@users.noreply.github.com>
2024-03-19 21:48:00 -0600

    reset

--- client/src/components/TodoInput.tsx
+++ client/src/components/TodoInput.tsx
@@ -6,9 +6,12 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { TodoFormData } from '../types/todo';
 
 export const TodoForm = () => {
-    const { register, handleSubmit } = useForm<TodoFormData>()
-    const { mutate: addNewTodo } = useAddNewTodoMutation()
-    const onSubmit: SubmitHandler<TodoFormData> = (data) => addNewTodo(data)
+    const { register, handleSubmit, reset } = useForm<TodoFormData>()
+    const { mutate: addNewTodo  } = useAddNewTodoMutation()
+    const onSubmit: SubmitHandler<TodoFormData> = (data) => {
+        addNewTodo(data)
+        reset({ text: '' })
+    }
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
